713d487e5ad74e842efb5e915552cd2c345d9842
BigKamil <allonder1992@gmail.com>
2021-09-28 00:09:26 +0200

    Add tests api project to the solution

---
+++ TestsApi/Startup.cs
@@ -0,0 +1,47 @@
+﻿using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+using System.Web.Http;
+using Microsoft.Owin;
+using Microsoft.Owin.Cors;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Serialization;
+using Owin;
+using TestsApi;
+
+[assembly: OwinStartup(typeof(Startup))]
+namespace TestsApi
+{
+    public class Startup
+    {
+        public void Configuration(IAppBuilder builder)
+        {
+            var httpConfiguration = CreateHttpConfiguration();
+
+            builder
+                .UseCors(CorsOptions.AllowAll)
+                .UseWebApi(httpConfiguration);
+        }
+
+        private HttpConfiguration CreateHttpConfiguration()
+        {
+            var config = new HttpConfiguration();
+            
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                "DefaultApi",
+                "api/{controller}/{id}",
+                new {id = RouteParameter.Optional}
+            );
+            
+            config.Formatters.Add(new JsonMediaTypeFormatter());
+            config.Formatters.JsonFormatter.SerializerSettings.Formatting = Formatting.Indented;
+            config.Formatters.JsonFormatter.SerializerSettings.ContractResolver =
+                new CamelCasePropertyNamesContractResolver();
+            config.Formatters.XmlFormatter.SupportedMediaTypes.Add(
+                new MediaTypeHeaderValue("multipart/form-data"));
+
+            return config;
+        }
+    }
+}
