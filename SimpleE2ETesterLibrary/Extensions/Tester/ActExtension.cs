f6e02702feaf3f28084d4fcb5cc9486026ef2a53
kk08 <kk08@pop-os.localdomain>
2021-12-21 12:13:37 +0100

    Merge Act and ActAsync
    Remove ActAsync extension

--- SimpleE2ETesterLibrary/Extensions/Tester/ActExtension.cs
+++ SimpleE2ETesterLibrary/Extensions/Tester/ActExtension.cs
@@ -7,20 +7,38 @@ namespace SimpleE2ETesterLibrary.Extensions.Tester
 {
     public static class ActExtension
     {
-        public static ISimpleE2ETester Act(this ISimpleE2ETester tester, Action action)
+        public static ISimpleE2ETester Act(this ISimpleE2ETester task, Action action)
         {
+            action();
+
+            return task;
+        }
+
+        public static async Task<ISimpleE2ETester> Act(this ISimpleE2ETester task, Func<Task> func)
+        {
+            await func();
+
+            return task;
+        }
+
+
+        public static async Task<ISimpleE2ETester> Act(this Task<ISimpleE2ETester> task, Func<Task> func)
+        {
+            await func();
+
+            return await task;
+        }
+
+        public static async Task<ISimpleE2ETester> Act(this Task<ISimpleE2ETester> task, Action action)
+        {
+            var tester = await task;
+
             action();
 
             return tester;
         }
 
-        public static Task<ISimpleE2ETester> Act(this Task<ISimpleE2ETester> testerTask, Action action)
-        {
-            var tester = testerTask.GetAwaiter().GetResult();
 
-            action();
 
-            return Task.FromResult(tester);
-        }
     }
 }
