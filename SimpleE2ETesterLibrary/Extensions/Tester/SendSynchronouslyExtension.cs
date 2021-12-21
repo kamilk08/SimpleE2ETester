4b5a78d64aca583f97f9c75d8b875d6224146c17
kk08 <kk08@pop-os.localdomain>
2021-12-21 12:43:40 +0100

    Add missing methods in SendSynchronously extension

--- SimpleE2ETesterLibrary/Extensions/Tester/SendSynchronouslyExtension.cs
+++ SimpleE2ETesterLibrary/Extensions/Tester/SendSynchronouslyExtension.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using SimpleE2ETesterLibrary.Interfaces;
 
 namespace SimpleE2ETesterLibrary.Extensions.Tester
@@ -14,9 +15,29 @@ namespace SimpleE2ETesterLibrary.Extensions.Tester
             return tester;
         }
 
+        public static ISimpleE2ETester SendSynchronously(this ISimpleE2ETester tester, Func<ISimpleHttpRequest> func)
+        {
+            tester.SendSynchronously(func());
+
+            return tester;
+        }
+
         public static Task<ISimpleE2ETester> SendSynchronously(this Task<ISimpleE2ETester> task,
             ISimpleHttpRequest request)
         {
+            var tester = task.GetAwaiter().GetResult();
+
+            var response = tester.Client.SendAsync(request).GetAwaiter().GetResult();
+
+            tester.AddCompletedRequest(request, response);
+
+            return Task.FromResult(tester);
+        }
+
+        public static Task<ISimpleE2ETester> SendSynchronously(this Task<ISimpleE2ETester> task, Func<ISimpleHttpRequest> func)
+        {
+            var request = func();
+
             var tester = task.GetAwaiter().GetResult();
 
             var response = tester.Client.SendAsync(request).GetAwaiter().GetResult();
